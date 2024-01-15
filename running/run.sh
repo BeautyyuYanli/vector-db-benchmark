3f7979e6cfaa653991ad1cd8551ad8e157704dae
盐粒 Yanli <mail@yanli.one>
2024-01-15 14:52:11 +0800

    adapt complex benchmark

--- running/run.sh
+++ running/run.sh
@@ -4,4 +4,4 @@ poetry install --no-interaction --no-root
 LATEST_STABLE_VERSION=$(gh release list --repo tensorchord/pgvecto.rs --exclude-drafts --exclude-pre-releases --limit 1 | awk '{print $3}')
 docker remove -f pgvecto-rs-demo || true
 docker run --rm --name pgvecto-rs-demo -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=123456 -d tensorchord/pgvecto-rs:pg15-${LATEST_STABLE_VERSION}
-poetry run python run.py --engines "*pgvector*"
+poetry run python run.py --engines "*pgvecto_rs*"
--- scripts/handle_price.py
+++ scripts/handle_price.py
@@ -4,11 +4,10 @@ from typing import Iterator
 from common import walk_result_file_paths
 
 
-
-for file in walk_result_file_paths(root_path="../results"):
-    with open(file, 'r') as f:
+for file in walk_result_file_paths(root_path="results"):
+    with open(file, "r") as f:
         data = json.load(f)
-    data['meta']['monthly_cost'] = 208.82
+    data["meta"]["monthly_cost"] = 0
 
-    with open(file, 'w') as f:
+    with open(file, "w") as f:
         json.dump(data, f, indent=2)
