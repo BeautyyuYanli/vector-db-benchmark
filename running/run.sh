63d2b614c273872b4def2369b610046a2362d287
盐粒 Yanli <mail@yanli.one>
2024-01-18 15:42:53 +0800

    remove submodule && update run.sh

--- running/run.sh
+++ running/run.sh
@@ -5,3 +5,14 @@ LATEST_STABLE_VERSION=$(gh release list --repo tensorchord/pgvecto.rs --exclude-
 docker remove -f pgvecto-rs-demo || true
 docker run --rm --name pgvecto-rs-demo -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=123456 -d tensorchord/pgvecto-rs:pg15-${LATEST_STABLE_VERSION}
 poetry run python run.py --engines "*pgvecto_rs*"
+
+python3 scripts/handle_price.py
+python3 scripts/process_result_files.py
+
+mv benchmark.json visualization/public/benchmark.json
+cd visualization
+git pull origin main
+git add .
+git commit -m "update benchmark.json $(date -Iminutes)"
+git push origin HEAD:main
+cd ..
