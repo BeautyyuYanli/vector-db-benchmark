2d15ed2f033c0f167f1fdc9473fa8e8897750e3b
盐粒 Yanli <mail@yanli.one>
2024-01-19 20:40:51 +0800

    generate configs

---
+++ running/generate_configs.py
@@ -0,0 +1,118 @@
+from msgspec import Struct, field, to_builtins
+from typing import (
+    List,
+)
+import itertools
+import json
+import argparse
+
+
+class ConnectionParam(Struct, kw_only=True):
+    host: str = "localhost"
+    port: int = 5432
+    user: str = "root"
+    password: str = "123456"
+
+
+class SearchParam(Struct, kw_only=True):
+    parallel: int
+    top: int
+    params: dict
+
+
+class UploadParam(Struct, kw_only=True):
+    parallel: int = 16
+    batch_size: int = 64
+    index_params: dict
+    index_type: str
+    engine_type: str = "rust"
+
+
+class EngineParam(Struct, kw_only=True):
+    name: str
+    result_group: str = "single_search"
+    engine: str
+    platform: str = "unknown"
+    index_type: str
+    dataset: str = "laion-768-1m-ip"
+    version: str
+    branch: str = "unknown"
+    commit: str = "unknown"
+    remark: str = "none"
+    other: str = "none"
+    link: str = "https://github.com/tensorchord/pgvecto.rs"
+    connection_params: ConnectionParam = field(default_factory=ConnectionParam)
+    collection_params: dict = field(default_factory=dict)
+    search_params: List[SearchParam]
+    upload_params: UploadParam
+
+
+SEARCH_PARALLEL = [16]
+SEARCH_TOP = [10]
+SEARCH_PARAMS = [
+    {"vectors.hnsw_ef_search": 8},
+    {"vectors.hnsw_ef_search": 16},
+    {"vectors.hnsw_ef_search": 32},
+    {"vectors.hnsw_ef_search": 64},
+    {"vectors.hnsw_ef_search": 128},
+    {"vectors.hnsw_ef_search": 256},
+    {"vectors.hnsw_ef_search": 512},
+]
+INDEX_PARAMS = [
+    (
+        "hnsw",
+        {
+            "indexing": {
+                "hnsw": {
+                    "m": 12,
+                    "ef_construction": 100,
+                }
+            },
+        },
+    ),
+    (
+        "flat",
+        {
+            "indexing": {"flat": {}},
+        },
+    ),
+]
+
+
+def generate(version: str):
+    return [
+        EngineParam(
+            name=f"pgvecto.rs-{version}-{index_params[0]}",
+            engine="pgvector",
+            index_type=index_params[0],
+            version=version,
+            search_params=[
+                SearchParam(
+                    parallel=search_parallel,
+                    top=search_top,
+                    params=search_params,
+                )
+                for search_parallel, search_top, search_params in itertools.product(
+                    SEARCH_PARALLEL, SEARCH_TOP, SEARCH_PARAMS
+                )
+            ],
+            upload_params=UploadParam(
+                index_params=index_params[1],
+                index_type=index_params[0],
+            ),
+        )
+        for index_params in INDEX_PARAMS
+    ]
+
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--version", type=str)
+args = arg_parser.parse_args()
+version = args.version
+
+with open(f"experiments/configurations/pgvecto.rs-{version}.json", "w") as f:
+    json.dump(
+        [to_builtins(engine) for engine in generate(version)],
+        f,
+        indent=4,
+    )
--- running/run.sh
+++ running/run.sh
@@ -4,7 +4,8 @@ poetry install --no-interaction --no-root
 LATEST_STABLE_VERSION=$(gh release list --repo tensorchord/pgvecto.rs --exclude-drafts --exclude-pre-releases --limit 1 | awk '{print $3}')
 docker remove -f pgvecto-rs-demo || true
 docker run --rm --name pgvecto-rs-demo -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=123456 -d tensorchord/pgvecto-rs:pg15-${LATEST_STABLE_VERSION}
-poetry run python run.py --engines "*pgvecto_rs*"
+poetry run python running/generate_configs.py
+poetry run python run.py --engines "*pgvecto.rs-$LATEST_STABLE_VERSION*"
 
 python3 scripts/handle_price.py
 python3 scripts/process_result_files.py
